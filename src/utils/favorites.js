4764633c70e491f3dafaee464bb442e7d968acea
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-08-19 00:25:11 +0300

    fix

--- src/utils/favorites.js
+++ src/utils/favorites.js
@@ -1,6 +1,5 @@
-const savedFavorites = JSON.parse(window.localStorage.getItem('favorites'));
-
 export const getFavorites = () => {
+  const savedFavorites = JSON.parse(window.localStorage.getItem('favorites'));
   if (savedFavorites === null) {
     return [];
   } else {
@@ -9,12 +8,14 @@ export const getFavorites = () => {
 };
 
 export const removeFromFavorites = id => {
+  const savedFavorites = getFavorites();
   const index = savedFavorites.findIndex(item => item._id === id);
   savedFavorites.splice(index, 1);
   window.localStorage.setItem('favorites', JSON.stringify(savedFavorites));
 };
 
 export const addToFavorites = advert => {
+  const savedFavorites = getFavorites();
   savedFavorites.push(advert);
   window.localStorage.setItem('favorites', JSON.stringify(savedFavorites));
 };
