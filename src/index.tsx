9d4702a17f9ab33f2d9b5c779ca99817d875a679
HannaPalianytsia <hannapalianytsia1@gmail.com>
2024-09-21 16:13:04 +0200

    migration-ts-try

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './components/App.jsx';
+import { App } from './components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter basename="/CamperGoApp">
